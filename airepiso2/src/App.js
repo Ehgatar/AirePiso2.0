425a9b7878a9b10efe7dc909afb3bc68dc9cca6e
Edgar Castellanos <edgarcast7@gmail.com>
2018-05-12 12:00:44 -0500

    adds a bunch of non-working particlesjs files, worked with hadi. dont use this broken branch

--- airepiso2/src/App.js
+++ airepiso2/src/App.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 //import './sass';
 import Home from './components/Home/Home';
 import About from "./components/pages/About"
 import Headers from "./components/Headers/Headers"
 import {BrowserRouter as Router,Route, Switch} from 'react-router-dom' 
-
+// import particlesJS from './public/js/particlesJS'
+// particlesJS.load('particles-js', './public/js/particles.json', function() {
+//   console.log('callback - particles.js config loaded');
+// });
+import Particles from 'react-particles-js';
 
 
 const App = () => (
   <Router>
     <div>
+    <Particles />
     <Headers />
     <Switch>
       <Route exact path="/" component={Home} />
